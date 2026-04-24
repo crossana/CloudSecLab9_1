a5a639b737b58dacbfeaf6ccb6b2888ace5e5867
Andrew <a.crossan@surrey>
2026-04-24 11:31:29 +0000

    removed whitespace

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
 # import the elements from the Flask framework 
-
 from flask import Flask, render_template 
  
 
 # Our web application object 
-
 app = Flask(__name__) 
-
- 
 
 # Provide the code to handle the root route into our application 
 
 @app.route('/') 
-
 def index(): 
 
     return render_template('index.html') 
-
- 
 
 # Run the webserver on port 8080 
 
